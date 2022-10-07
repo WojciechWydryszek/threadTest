eb7e772c03b0d8a532720606f30fc0135347bea9
Wojciech Wydryszek <wojciech.wydryszek@uvic.cat>
2022-10-07 16:23:16 +0200

    first commit

---
+++ app/src/main/java/cat/uvic/teknos/m09/Dummy.java
@@ -0,0 +1,18 @@
+package cat.uvic.teknos.m09;
+
+public class Dummy {
+    public static void printSomething1()
+    {
+        System.out.println("Print something One");
+    }
+
+    public static void printSomething2()
+    {
+        System.out.println("Print something Two");
+    }
+
+    public static void printSomething3()
+    {
+        System.out.println("Print something Three");
+    }
+}
